9872fb1f0a6ed851c698531294c51f44ffe326f4
Hiram <hiram@ThinkPadX13.Yeico>
2021-12-04 14:56:45 +0000

    se meten los rows como funcion

--- lv-react-ui/src/Utils.js
+++ lv-react-ui/src/Utils.js
@@ -1,20 +1,52 @@
-import './App.css';
-import Button from 'react-bootstrap/Button'
+import { useState } from 'react'
 
+import './App.css';
+import api from "./api"
+import Button from 'react-bootstrap/Button'
+import Table from 'react-bootstrap/Table'
 import React from "react"
 
-function handleDiscovery() {
-  fetch("discovery")
-  .then(res => res.json())
-  .then(msg => console.log(msg))
-}
-
 function Utils() {
+
+  const [devices, setDevices] = useState([])
+  const [isDataDevice, setIsDataDevice] = React.useState(false)
+
+  function buttonDiscoveryClick() {
+    api.getNetworkDiscover(function (res) {
+      console.log(JSON.stringify(res))
+      setDevices(res)
+    })
+  }
+
+  function rows() {
+    console.log(devices)
+    return devices.map(d => <tr key="d.data.macaddr">
+      <td>{d.data.macaddr}</td>
+    </tr>)
+  }
+
   return (
-    <div>
-      <Button onClick={handleDiscovery}>Discovery</Button>
-    </div>
-  );
-} 
+    <>
+      <Button variant="primary" onClick={buttonDiscoveryClick}>
+        Discovery
+      </Button>
+      <Table striped bordered hover variant="dark">
+        <thead>
+          <tr>
+            <th>#</th>
+            <th>First Name</th>
+            <th>Last Name</th>
+            <th>Username</th>
+          </tr>
+        </thead>
+        <tbody>
+          {rows()}
+        </tbody>
+      </Table>
+
+    </>
+  )
+
+}
 
 export default Utils;
--- lv-react-ui/src/api.js
+++ lv-react-ui/src/api.js
@@ -1,44 +1,53 @@
 function setNetworkConfigDhcp(config, cb) {
-    fetch("/net/setup/eth0", {
-        method: "post",
-        body: JSON.stringify(config),
-        headers: { "Content-Type": "application/json" }
-      })
-      .then(res => res.json())
-      .then(json => cb(json))
-      .catch(err => cb(err))
+  fetch("/net/setup/eth0", {
+    method: "post",
+    body: JSON.stringify(config),
+    headers: { "Content-Type": "application/json" }
+  })
+    .then(res => res.json())
+    .then(json => cb(json))
+    .catch(err => cb(err))
 }
 
-function setNetworkConfigStatic(config, cb){
-    fetch("/net/setup/eth0", {
-        method: "post",
-        body: JSON.stringify(config),
-        headers: { "Content-Type": "application/json" }
-      })
-      .then(res => res.json())
-      .then(json => cb(json))
-      .catch(err => cb(err))
+function setNetworkConfigStatic(config, cb) {
+  fetch("/net/setup/eth0", {
+    method: "post",
+    body: JSON.stringify(config),
+    headers: { "Content-Type": "application/json" }
+  })
+    .then(res => res.json())
+    .then(json => cb(json))
+    .catch(err => cb(err))
 }
 
 function getNetworkConfig(cb) {
-    fetch("/net/state/eth0")
-      .then(res => res.json())
-      .then(json => cb(json))
-      .catch(err => cb(err))
+  fetch("/net/state/eth0")
+    .then(res => res.json())
+    .then(json => cb(json))
+    .catch(err => cb(err))
 }
 
 function getNetworkPing(cb) {
-    fetch("/ping")
-      .then(res => res.json())
-      .then(json => cb(json))
-      .catch(err => cb(err))
+  fetch("http://10.77.3.170:31680/ping")
+    .then(res => res.json())
+    .then(json => cb(json))
+    .catch(err => cb(err))
+}
+
+
+function getNetworkDiscover(cb) {
+  fetch("discovery/2")
+    .then(res => res.json())
+    .then(json => cb(json))
+    .catch(err => cb(err))
 }
 
 const exports = {
-    setNetworkConfigDhcp,
-    getNetworkConfig,
-    getNetworkPing, 
-    setNetworkConfigStatic,
+  setNetworkConfigDhcp,
+  getNetworkConfig,
+  getNetworkPing,
+  setNetworkConfigStatic,
+  getNetworkDiscover,
 }
 
 export default exports
