3cec381a2d8c61e32c3c13ced0c5c3cb361dbbd2
Hiram Zuñiga <hiram.zuniga@yeico.com>
2022-01-07 16:07:21 -0600

    se mueven repos

---
+++ lv-react-ui/build/precache-manifest.028a1b5ddc39ac7cd9eb347000c3ebcb.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "565ef01decf5d3b4f928f6332bd179b6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "02adc2fbc76d7a9fe9b3",
+    "url": "/static/css/2.fa43bc9e.chunk.css"
+  },
+  {
+    "revision": "abc1b293e632fbb5a581",
+    "url": "/static/css/main.dc1a3561.chunk.css"
+  },
+  {
+    "revision": "02adc2fbc76d7a9fe9b3",
+    "url": "/static/js/2.33c52a3c.chunk.js"
+  },
+  {
+    "revision": "435881bb28fec4d4e84baf9dfd194218",
+    "url": "/static/js/2.33c52a3c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8013e08bb244840a87bc",
+    "url": "/static/js/3.407da2c6.chunk.js"
+  },
+  {
+    "revision": "abc1b293e632fbb5a581",
+    "url": "/static/js/main.3d326065.chunk.js"
+  },
+  {
+    "revision": "54211c6c08ee9060b384",
+    "url": "/static/js/runtime-main.84b243f3.js"
+  }
+]);
