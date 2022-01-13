2243e927a8a251937eb00e88d0558e14b8ef90f0
Hiram Zuñiga <hiram.zuniga@yeico.com>
2022-01-13 13:02:28 -0600

    empezando tour ahora se pueden ir ejecutando cada uno sin errores

---
+++ go/Atourofgo/go.mod
@@ -0,0 +1,3 @@
+module github.com/hiramzunig4/tryouts/go/Atourofgo
+
+go 1.17
---
+++ go/Atourofgo/main.go
@@ -0,0 +1,10 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	fmt.Println("File Pointers.go run")
+	pointers()
+}
